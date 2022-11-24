cd743d2a261910ceae98f7c07b0f8f6e71ab37e2
Manuel Resinas <resinas@us.es>
2022-11-24 11:14:26 +0100

    Improves evaluation results generation and robustness

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+import time
 from ppinat.input import input_test
 import json
 import pandas as pd
@@ -6,6 +8,7 @@ import re
 import logging
 
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(module)s:%(funcName)s:%(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def compute_precision_recall(overall, identified, goldstandard, allow_regular=False):
     found = overall['good'] + \
@@ -15,6 +18,39 @@ def compute_precision_recall(overall, identified, goldstandard, allow_regular=Fa
         "precision": found / identified if identified > 0 else None,
         "recall": found / goldstandard if goldstandard > 0 else None
     }
+
+def create_search_grid(grid_spec):
+    processed_matching = {}
+    model_comb = [(x, y, z) for x in grid_spec["is"] for y in grid_spec["emb"] for z in grid_spec["bart"] if x + y + z == 1]
+    for (x, y, z) in model_comb:
+        for complete in grid_spec["complete"]:
+            for att in grid_spec["att"]:
+                one_slot = {
+                    "slot_is_sim": x * (1-att) * (1 - complete),
+                    "slot_complete_is_sim": x * (1-att) * complete,
+                    "slot_emb": y * (1-att) * (1 - complete),
+                    "slot_complete_emb": y * (1-att) * (complete),
+                    "bart_large_mnli_personalized": z * (1-att) * (1-complete),
+                    "bart_large_mnli_personalized_complete": z * (1-att) * complete,
+                    "att_is_sim": att * (1 - complete),
+                    "att_complete_is_sim": att * (complete)
+                }
+
+                for multi_heur in grid_spec["multi_heur"]:
+                    multi_slot = {
+                        **({f"ev1_${k}": v/2*(1-multi_heur) for k,v in one_slot.items()}),
+                        **({f"ev2_${k}": v/2*(1-multi_heur) for k,v in one_slot.items()}),
+                        "same_type": multi_heur / 2,
+                        "condition_ratio": multi_heur / 2
+                    }
+                    name = f"is_{x}__emb_{y}__bart_{z}__c_{complete}__att_{att}__mh_{multi_heur}"
+
+                    processed_matching[name] = {
+                        "one_slot": one_slot,
+                        "multi_slot": multi_slot
+                    }
+
+    return processed_matching    
 
 
 parser = argparse.ArgumentParser()
@@ -57,37 +93,7 @@ parsing = config["parsing"]
 matching = config["matching"]
 
 if "hs" in matching:
-    processed_matching = {}
-    model_comb = [(x, y, z) for x in matching["hs"]["is"] for y in matching["hs"]["emb"] for z in matching["hs"]["bart"] if x + y + z == 1]
-    for (x, y, z) in model_comb:
-        for complete in matching["hs"]["complete"]:
-            for att in matching["hs"]["att"]:
-                one_slot = {
-                    "slot_is_sim": x * (1-att) * (1 - complete),
-                    "slot_complete_is_sim": x * (1-att) * complete,
-                    "slot_emb": y * (1-att) * (1 - complete),
-                    "slot_complete_emb": y * (1-att) * (complete),
-                    "bart_large_mnli_personalized": z * (1-att) * (1-complete),
-                    "bart_large_mnli_personalized_complete": z * (1-att) * complete,
-                    "att_is_sim": att * (1 - complete),
-                    "att_complete_is_sim": att * (complete)
-                }
-
-                for multi_heur in matching["hs"]["multi_heur"]:
-                    multi_slot = {
-                        **({f"ev1_${k}": v/2*(1-multi_heur) for k,v in one_slot.items()}),
-                        **({f"ev2_${k}": v/2*(1-multi_heur) for k,v in one_slot.items()}),
-                        "same_type": multi_heur / 2,
-                        "condition_ratio": multi_heur / 2
-                    }
-                    name = f"is_{x}__emb_{y}__bart_{z}__c_{complete}__att_{att}__mh_{multi_heur}"
-
-                    processed_matching[name] = {
-                        "one_slot": one_slot,
-                        "multi_slot": multi_slot
-                    }
-
-    matching = processed_matching
+    matching = create_search_grid(matching["hs"])
 
 for dataset in datasets:
     parsing_metrics_results = []
@@ -98,37 +104,46 @@ for dataset in datasets:
     for parsing_model in parsing:
 
         print(f"---------- {parsing_model}  ----------")
-        test_execution = input_test.TestExecution(
-            args=args, dataset=dataset, parsing_model=parsing_model, matching_models = matching)
 
-            
-        for matching_model in test_execution.result:
-            test = test_execution.result[matching_model]
+        try:
+            test_execution = input_test.TestExecution(
+                args=args, dataset=dataset, parsing_model=parsing_model, matching_models = matching)
 
-            parsing_metrics_results.append({
-                "parsing_type": parsing_model,
-                "matching_type": matching_model,
-                **test.tagging_overall
-            })
-            update_results(parsing_tags_results, test.tagging_tag, parsing_type=parsing_model, matching_type=matching_model)
+                
+            for matching_model in test_execution.result:
+                test = test_execution.result[matching_model]
 
-            matching_metrics_results.append({
-                "parsing_type": parsing_model,
-                "matching_type": matching_model,
-                **test.matching_overall
-            })
-            update_results(matching_attrib_results, test.matching_attribute, parsing_type=parsing_model, matching_type=matching_model)
+                parsing_metrics_results.append({
+                    "parsing_type": parsing_model,
+                    "matching_type": matching_model,
+                    **test.tagging_overall
+                })
+                update_results(parsing_tags_results, test.tagging_tag, parsing_type=parsing_model, matching_type=matching_model)
+
+                matching_metrics_results.append({
+                    "parsing_type": parsing_model,
+                    "matching_type": matching_model,
+                    **test.matching_overall
+                })
+                update_results(matching_attrib_results, test.matching_attribute, parsing_type=parsing_model, matching_type=matching_model)
+        except Exception as e:
+            logger.exception(f"Failed test execution of {parsing_model} in {dataset}", exc_info=e)
 
     dataset_name = re.findall("./input/(.*)", dataset)[0]
 
+    if not os.path.exists("results"):
+        os.mkdir("results")
+
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+
     df = pd.DataFrame(parsing_tags_results)
-    df.to_csv(f"{dataset_name}-parsing-tags-results.csv")
+    df.to_csv(f"results/{timestr}-{dataset_name}-parsing-tags-results.csv")
     pm = pd.DataFrame(parsing_metrics_results)
-    pm.to_csv(f"{dataset_name}-parsing-metrics-results.csv")
+    pm.to_csv(f"results/{timestr}-{dataset_name}-parsing-metrics-results.csv")
     mm = pd.DataFrame(matching_metrics_results)
-    mm.to_csv(f"{dataset_name}-matching-metrics-results.csv")
+    mm.to_csv(f"results/{timestr}-{dataset_name}-matching-metrics-results.csv")
     df = pd.DataFrame(matching_attrib_results)
-    df.to_csv(f"{dataset_name}-matching-attrib-results.csv")
+    df.to_csv(f"results/{timestr}-{dataset_name}-matching-attrib-results.csv")
 
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
         print("OVERALL PARSING RESULTS:")
