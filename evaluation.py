662da30426941dc52712b7d44f3286670bcf5262
Manuel Resinas <resinas@us.es>
2022-11-25 09:27:21 +0100

    Automated generation of weights in evaluation

--- evaluation.py
+++ evaluation.py
@@ -25,32 +25,36 @@ def create_search_grid(grid_spec):
     for (x, y, z) in model_comb:
         for complete in grid_spec["complete"]:
             for att in grid_spec["att"]:
-                one_slot = {
-                    "slot_is_sim": x * (1-att) * (1 - complete),
-                    "slot_complete_is_sim": x * (1-att) * complete,
-                    "slot_emb": y * (1-att) * (1 - complete),
-                    "slot_complete_emb": y * (1-att) * (complete),
-                    "bart_large_mnli_personalized": z * (1-att) * (1-complete),
-                    "bart_large_mnli_personalized_complete": z * (1-att) * complete,
-                    "att_is_sim": att * (1 - complete),
-                    "att_complete_is_sim": att * (complete)
-                }
-
                 for multi_heur in grid_spec["multi_heur"]:
-                    multi_slot = {
-                        **({f"ev1_${k}": v/2*(1-multi_heur) for k,v in one_slot.items()}),
-                        **({f"ev2_${k}": v/2*(1-multi_heur) for k,v in one_slot.items()}),
-                        "same_type": multi_heur / 2,
-                        "condition_ratio": multi_heur / 2
-                    }
                     name = f"is_{x}__emb_{y}__bart_{z}__c_{complete}__att_{att}__mh_{multi_heur}"
-
-                    processed_matching[name] = {
-                        "one_slot": one_slot,
-                        "multi_slot": multi_slot
-                    }
+                    processed_matching[name] = generate_weights(iss=x, emb=y, bart=z, att=att, complete=complete, multi_heur=multi_heur)
 
     return processed_matching    
+
+def generate_weights(iss=0, emb=0, bart=0, att=0, complete=0, multi_heur=0):
+    one_slot = {
+        "slot_is_sim": iss * (1-att) * (1 - complete),
+        "slot_complete_is_sim": iss * (1-att) * complete,
+        "slot_emb": emb * (1-att) * (1 - complete),
+        "slot_complete_emb": emb * (1-att) * (complete),
+        "bart_large_mnli_personalized": bart * (1-att) * (1-complete),
+        "bart_large_mnli_personalized_complete": bart * (1-att) * complete,
+        "att_is_sim": att * (1 - complete),
+        "att_complete_is_sim": att * (complete)
+    }
+
+    multi_slot = {
+        **({f"ev1_${k}": v/2*(1-multi_heur) for k,v in one_slot.items()}),
+        **({f"ev2_${k}": v/2*(1-multi_heur) for k,v in one_slot.items()}),
+        "same_type": multi_heur / 2,
+        "condition_ratio": multi_heur / 2
+    }
+
+    return {
+        "one_slot": one_slot,
+        "multi_slot": multi_slot
+    }
+
 
 
 parser = argparse.ArgumentParser()
@@ -94,6 +98,10 @@ matching = config["matching"]
 
 if "hs" in matching:
     matching = create_search_grid(matching["hs"])
+else:
+    for key in matching:
+        if "$gen" in matching["key"]:
+            matching["key"] = generate_weights(**matching["key"]["$gen"])
 
 for dataset in datasets:
     parsing_metrics_results = []
