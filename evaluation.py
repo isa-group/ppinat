713e4e9ad1ebf9647883353bb67cf2eb4c766762
Manuel Resinas <resinas@us.es>
2022-11-25 22:56:22 +0100

    Add slot_sim to grid search

--- evaluation.py
+++ evaluation.py
@@ -21,18 +21,20 @@ def compute_precision_recall(overall, identified, goldstandard, allow_regular=Fa
 
 def create_search_grid(grid_spec):
     processed_matching = {}
-    model_comb = [(x, y, z) for x in grid_spec["is"] for y in grid_spec["emb"] for z in grid_spec["bart"] if x + y + z == 1]
-    for (x, y, z) in model_comb:
+    model_comb = [(x, y, z, t) for x in grid_spec["is"] for y in grid_spec["emb"] for z in grid_spec["bart"]  for t in grid_spec["vec"] if x + y + z + t == 1]
+    for (x, y, z, t) in model_comb:
         for complete in grid_spec["complete"]:
             for att in grid_spec["att"]:
                 for multi_heur in grid_spec["multi_heur"]:
-                    name = f"is_{x}__emb_{y}__bart_{z}__c_{complete}__att_{att}__mh_{multi_heur}"
-                    processed_matching[name] = generate_weights(iss=x, emb=y, bart=z, att=att, complete=complete, multi_heur=multi_heur)
+                    name = f"is_{x}__emb_{y}__bart_{z}__vec_{t}__c_{complete}__att_{att}__mh_{multi_heur}"
+                    processed_matching[name] = generate_weights(iss=x, emb=y, bart=z, vec=t, att=att, complete=complete, multi_heur=multi_heur)
 
     return processed_matching    
 
-def generate_weights(iss=0, emb=0, bart=0, att=0, complete=0, multi_heur=0):
+def generate_weights(iss=0, emb=0, bart=0, vec=0, att=0, complete=0, multi_heur=0):
     one_slot = {
+        "slot_sim": vec * (1-att) * (1 - complete),
+        "slot_complete_sim": vec * (1-att) * complete,
         "slot_is_sim": iss * (1-att) * (1 - complete),
         "slot_complete_is_sim": iss * (1-att) * complete,
         "slot_emb": emb * (1-att) * (1 - complete),
@@ -98,6 +100,7 @@ matching = config["matching"]
 
 if "hs" in matching:
     matching = create_search_grid(matching["hs"])
+    logger.info(f"Grid size: {len(matching)}")
 else:
     for key in matching:
         if "$gen" in matching[key]:
